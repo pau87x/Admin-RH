8d4434f16c77f50087034b39d8fa88b031e64695
Paulina Elizalde <pelizaldea@gmail.com>
2015-01-15 12:51:59 -0600

    Removing/Adding views

---
+++ app/views/attendance/report-list.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts')
+
+@section('extra-css')
+<link href="{{ asset('bootstrap-datepicker/css/datepicker3.css') }}" rel="stylesheet">
+<link href="{{ asset('bootstrap-select/dist/css/bootstrap-select.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+<h1 class="page-header">Reporte de Personal</h1>
+
+            {{ Form::open(['route' => 'report_list_search', 'method' => 'GET', 'role' => 'form', 'novalidate']) }}
+
+            {{ Field::text('date', null, ['id' => 'date', 'class'=>'datepicker','maxlength' => '10']) }}
+            
+            <p>
+                <input type="submit" value="Buscar" class="btn btn-success">
+            </p>
+
+            {{ Form::close() }}
+@stop
+
+@section('extra-js')
+<script src="{{ asset('bootstrap-datepicker/js/bootstrap-datepicker.js') }}"></script>
+<script src="{{ asset('bootstrap-datepicker/js/bootstrap-datepicker.es.js') }}"></script>
+<script src="{{ asset('bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('bootstrap-select/dist/js/i18n/defaults-es_CL.min.js') }}"></script>
+<script>
+$(document).ready(function(){ 
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        language: "es",
+        startView: 1,
+        todayHighlight: true,
+        todayBtn: true,
+        autoclose: true
+    });
+    $('.selectpicker').selectpicker();
+});
+</script>
+@endsection
---
+++ app/views/attendance/show-report-list.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts')
+
+@section('content')
+
+<h1 class="page-header">Lista de Asistencia {{ Input::get('date') }}</h1>
+
+<div class="table-responsive">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Código</th>
+        <!-- <th>Nombre Completo</th> -->
+        <!-- <th>Puesto</th> -->
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($employees as $employee)
+      <tr>
+        <td>{{ $employee->code }}</td>
+<!--         <td>@{{ $employee->full_name }}</td>
+        <td> @{{ $employee->title }} </td> -->
+        <td>
+          <div id="navigation">
+            @if ($employee->assistance)
+              <a class="btn btn-success glyphicon glyphicon-ok ajax-uncheck" id="{{$employee->id}}uncheck" href="" role="button"> Asistió</a>
+            @else
+              <a class="btn btn-default glyphicon glyphicon-unchecked ajax-check" id="{{$employee->id}}check" href="" role="button"> Marcar</a>
+            @endif
+          </div>
+        </td>
+
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  <div class="text-center">
+      @{{ $employees->links() }}
+  </div>
+
+</div>
+
+@stop
