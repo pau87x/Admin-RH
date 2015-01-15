8f509e1fabb73c3cc7c687576c4f9fb18660c2e3
Paulina Elizalde <pelizaldea@gmail.com>
2015-01-14 22:58:48 -0600

    Fixing sentence sql like laravel

--- app/AdminRH/Repositories/AttendanceRepo.php
+++ app/AdminRH/Repositories/AttendanceRepo.php
@@ -24,14 +24,9 @@ class AttendanceRepo extends BaseRepo {
     
     public function getAttendances($date)
     {
-        // $results = DB::select( DB::raw("SELECT * FROM some_table WHERE some_col = :somevariable"), array(
-        //    'somevariable' => $someVariable,
-        //  ));
-        //$date = '2015-01-14';
-        $sql = "select *, (SELECT a.id FROM attendances a where e.id=a.employee_id and DATE(created_at) = '".$date."' limit 1) as assistance from employees e";
-        $results = \DB::select( \DB::raw($sql));
-        //$results = \DB::select( \DB::raw($sql), array('date' => $date));
-        //dd($results);
+        $sql = "select *, (SELECT a.id FROM attendances a where e.id=a.employee_id and DATE(created_at) = :date limit 1) as assistance from employees e";
+        $results = \DB::select( \DB::raw($sql), array('date' => $date));
+        
         return $results;
     }
 
