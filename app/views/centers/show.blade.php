c5ffab6980300778b8ea7104541b7faea76b73c3
Paulina Elizalde <pelizaldea@gmail.com>
2015-01-19 17:32:32 -0600

    restricting create, edit, delete to admin for centers, titles

--- app/views/centers/show.blade.php
+++ app/views/centers/show.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 
 <h1 class="page-header">Centros de servicio
+  @if (is_admin())
   <a class="btn btn-success pull-right" href="{{ route('new_center') }}" role="button">Nuevo</a>
+  @endif
 </h1>
 <div class="table-responsive">
   <table class="table table-striped">
@@ -11,7 +13,9 @@
       <tr>
         <th>ID</th>
         <th>Nombre</th>
+        @if (is_admin())
         <th>Acciones</th>
+        @endif
       </tr>
     </thead>
     <tbody>
@@ -19,8 +23,12 @@
       <tr>
         <td>{{ $center->id }}</td>
         <td>{{ $center->name }}</td>
-        <td><a class="btn btn-success" href="{{ route('edit_center', [$center->id]) }}" role="button">Editar</a>
-          <a class="btn btn-danger" href="#" role="button">Eliminar</a></td>
+        @if (is_admin())
+        <td>
+          <a class="btn btn-success" href="{{ route('edit_center', [$center->id]) }}" role="button">Editar</a>
+          <a class="btn btn-danger" href="#" role="button">Eliminar</a>
+        </td>
+        @endif
       </tr>
       @endforeach
     </tbody>
--- app/views/titles/show.blade.php
+++ app/views/titles/show.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 
 <h1 class="page-header">Puestos
+  @if (is_admin())
   <a class="btn btn-success pull-right" href="{{ route('new_title') }}" role="button">Nuevo</a>
+  @endif
 </h1>
 <div class="table-responsive">
   <table class="table table-striped">
@@ -11,7 +13,9 @@
       <tr>
         <th>ID</th>
         <th>Nombre</th>
+        @if (is_admin())
         <th>Acciones</th>
+        @endif
       </tr>
     </thead>
     <tbody>
@@ -19,8 +23,11 @@
       <tr>
         <td>{{ $title->id }}</td>
         <td>{{ $title->name }}</td>
+        @if (is_admin())
         <td><a class="btn btn-success" href="{{ route('edit_title', [$title->id]) }}" role="button">Editar</a>
-          <a class="btn btn-danger" href="#" role="button">Eliminar</a></td>
+          <a class="btn btn-danger" href="#" role="button">Eliminar</a>
+        </td>
+        @endif
       </tr>
       @endforeach
     </tbody>
