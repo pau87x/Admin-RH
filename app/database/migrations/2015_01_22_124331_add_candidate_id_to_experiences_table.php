f8c93e7238522652a6dcbbf2f7a0b44c67721763
Paulina Elizalde <pelizaldea@gmail.com>
2015-01-22 13:59:32 -0600

    Add to migrations candidate_id ups :P

---
+++ app/database/migrations/2015_01_22_124331_add_candidate_id_to_experiences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddCandidateIdToExperiencesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('experiences', function(Blueprint $table)
+		{
+			$table->integer('candidate_id')->unsigned()->after('end');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('experiences', function(Blueprint $table)
+		{
+			$table->dropColumn('candidate_id');	
+		});
+	}
+
+}
